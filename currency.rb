c72786a331b61c6ed4b358f47871fc70c8f99643
jnmerchant <joemercnc@gmail.com>
2017-01-17 14:59:16 -0500

    equal and not equal operator overload working

--- currency.rb
+++ currency.rb
@@ -8,7 +8,7 @@ class Currency
   attr_accessor :amount
 
 
-  def =(other)
+  def ==(other)
     if @code == other.code && @amount == other.amount
       true
     else
@@ -24,4 +24,17 @@ class Currency
     end
   end
 
+end
+
+def main
+  #test ==
+  currency1 = Currency.new("EUR", 9)
+  currency2 = Currency.new("GBR", 7)
+
+  puts currency1 != currency2
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  main
 end
