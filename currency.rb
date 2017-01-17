6fea606b5e89ca363a5f284119cec75c2bee623f
jnmerchant <joemercnc@gmail.com>
2017-01-17 14:50:44 -0500

    Updated the currency and currency_converter files

--- currency.rb
+++ currency.rb
@@ -1,9 +1,27 @@
 class Currency
   def initialize(code, amount)
-    attr_read :read
-    attr_accessor :amount
-
     @code = code
     @amount = amount
   end
+
+  attr_reader :code
+  attr_accessor :amount
+
+
+  def =(other)
+    if @code == other.code && @amount == other.amount
+      true
+    else
+      false
+    end
+  end
+
+  def !=(other)
+    if @code != other.code or @amount != other.amount
+      true
+    else
+      false
+    end
+  end
+
 end
--- currency_converter.rb
+++ currency_converter.rb
@@ -4,6 +4,6 @@ class CurrencyConverter
     currency_date = {"GBR" => {"GBR" => 1.00, "USD" => 1.23997, "EUR" => 1.15818},
       "USD" => {"GBR" => 0.80647, "USD" => 1.00, "EUR" => 0.93404},
       "EUR" => {"GBR" => 0.86342, "USD" => 1.07062, "EUR" => 1.00}}
-
+    end
 
 end
