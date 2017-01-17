318523e72633c819be4ae67121183e0edfe9d5c1
jnmerchant <joemercnc@gmail.com>
2017-01-17 17:15:57 -0500

    added working currency_converter initializer

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,9 +1,15 @@
 class CurrencyConverter
-  def initialize(code)
-    #access this has for a rate = currency_data["GBR"]["EUR"]
-    currency_date = {"GBR" => {"GBR" => 1.00, "USD" => 1.23997, "EUR" => 1.15818},
-      "USD" => {"GBR" => 0.80647, "USD" => 1.00, "EUR" => 0.93404},
-      "EUR" => {"GBR" => 0.86342, "USD" => 1.07062, "EUR" => 1.00}}
-    end
+  def initialize(currency, code)
+  #access this has for a rate = currency_data["GBR"]["EUR"]
+  currency_date = {"GBR" => {"GBR" => 1.00, "USD" => 1.23997, "EUR" => 1.15818},
+    "USD" => {"GBR" => 0.80647, "USD" => 1.00, "EUR" => 0.93404},
+    "EUR" => {"GBR" => 0.86342, "USD" => 1.07062, "EUR" => 1.00}}
+
+  @currency = currency
+  @code = code
+  end
+
+  attr_reader :currency
+  attr_reader :code
 
 end
--- currency_main.rb
+++ currency_main.rb
@@ -3,21 +3,10 @@ require_relative 'currency_converter'
 require_relative 'currency_converter_error'
 
 def main
-  #test ==
-  currency1 = Currency.new("EUR", 15)
-  currency2 = Currency.new("EUR", 7)
+  currency1 = Currency.new("$10000")
+  converted = CurrencyConverter.new(currency1, "GBR")
 
-  currency3 = currency1 + currency2
-  puts currency3.code
-  puts currency3.amount
-
-  currency4 = currency1 * 2
-  puts currency4.amount
-
-  currency5 = Currency.new("$1.20")
-  puts currency5.code
-  puts currency5.amount
-
+  p converted
 
 end
 
