c9b7ef01776e3d345a0645621f0b29e91953c0eb
jnmerchant <joemercnc@gmail.com>
2017-01-17 22:26:24 -0500

    updated currency main for a few sample cases

--- currency_main.rb
+++ currency_main.rb
@@ -5,9 +5,15 @@ require_relative 'currency_converter_error'
 def main
   currency1 = Currency.new("GBR", 1000)
   p currency1
+  puts currency1.code
+  puts currency1.amount
 
   converted = CurrencyConverter.new(currency1, "EUR")
   p converted
+  p converted.currency
+  p converted.code
+
+  p currency1 + Currency.new("GBR", 2000)
 
 end
 
