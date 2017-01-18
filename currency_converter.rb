dfb0b11780aafa3ecb46e9cffe9d71023f6c62b4
jnmerchant <joemercnc@gmail.com>
2017-01-17 21:11:05 -0500

    added currency_converter functionality to convert currencies

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,7 +1,7 @@
 class CurrencyConverter
   def initialize(currency, code)
   #access this has for a rate = currency_data["GBR"]["EUR"]
-  currency_date = {"GBR" => {"GBR" => 1.00, "USD" => 1.23997, "EUR" => 1.15818},
+  @currency_data = {"GBR" => {"GBR" => 1.00, "USD" => 1.23997, "EUR" => 1.15818},
     "USD" => {"GBR" => 0.80647, "USD" => 1.00, "EUR" => 0.93404},
     "EUR" => {"GBR" => 0.86342, "USD" => 1.07062, "EUR" => 1.00}}
 
@@ -11,5 +11,10 @@ class CurrencyConverter
 
   attr_reader :currency
   attr_reader :code
+
+  def convert_currency
+    converted_amount = @currency.amount.to_f * @currency_data[@currency.code][@code]
+    Currency.new(@code, converted_amount)
+  end
 
 end
--- currency_main.rb
+++ currency_main.rb
@@ -4,9 +4,12 @@ require_relative 'currency_converter_error'
 
 def main
   currency1 = Currency.new("$10000")
-  converted = CurrencyConverter.new(currency1, "GBR")
+  converted = CurrencyConverter.new(currency1, "USD")
 
-  p converted
+  currency6 = converted.convert_currency
+  p currency6
+  p currency6.code
+  p currency6.amount
 
 end
 
