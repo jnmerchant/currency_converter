a8538fe72a9d20c42de0c94d79268cee1d5b439b
jnmerchant <joemercnc@gmail.com>
2017-01-17 22:20:10 -0500

    updated main for sample cases

--- currency_main.rb
+++ currency_main.rb
@@ -3,7 +3,11 @@ require_relative 'currency_converter'
 require_relative 'currency_converter_error'
 
 def main
-  
+  currency1 = Currency.new("GBR", 1000)
+  p currency1
+
+  converted = CurrencyConverter.new(currency1, "EUR")
+  p converted
 
 end
 
